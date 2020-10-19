f873734e2c3e2e68554b7144d1bf914b506f5b9c
Ricky Chu <qs4869@outlook.com>
2020-10-19 09:49:07 +0800

    创建Token请求返回模型

---
+++ Models/RequestModel/TokenRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTest.Models.ResponseModel
+{
+    // Token请求返回模型
+    public class TokenResponseModel : BaseResponseModel
+    {
+        public string Access_Token { get; set; }
+        public int Expires_in { get; set; }
+    }
+}
