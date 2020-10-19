b47e74682606b73d4dc44fe6a38b9254deb9c99b
Ricky Chu <qs4869@outlook.com>
2020-10-19 09:52:24 +0800

    新增父类请求返回模型

---
+++ Models/ResponseModel/BaseResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTest.Models.ResponseModel
+{
+    // 父类请求返回模型
+    public class BaseResponseModel
+    {
+        public int Errcode { get; set; }
+        public string Errmsg { get; set; }
+    }
+}
