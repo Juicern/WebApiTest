da2039a82c8045e02dc5ebca5fb1688e308bef9e
Ricky Chu <qs4869@outlook.com>
2020-10-19 09:54:31 +0800

    创建部门请求返回模型

---
+++ Models/ResponseModel/DepartmentResponseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTest.Models.ResponseModel
+{
+    // 部门请求返回模型
+    public class DepartmentReponseModel : BaseResponseModel
+    {
+        public List<Department> Department { get; set; }
+    }
+
+    public class Department
+    {
+        public long ID { get; set; }
+        public bool AutoAddUser { get; set; }
+        public string Name { get; set; }
+        public bool CreateDeptGroup { get; set; }
+        public string Ext { get; set; }
+    }
+}
